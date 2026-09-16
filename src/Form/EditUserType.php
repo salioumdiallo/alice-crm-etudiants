359dd6658b5487cfa18f25d578c8ce1a8effb56c
Mamadou saliou diallo <salioumdiallo@gmail.com>
2026-09-16 11:57:42 +0200

    refactor: clarify user security roles and business status

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -4,15 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class EditUserType extends AbstractType
 {
@@ -21,67 +21,65 @@ class EditUserType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Email',
-                'disabled' => true
+                'disabled' => true,
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
                 'constraints' => [
                     new Length([
-                        'min' => 2, 
-                        'minMessage' => 'Le Prénom contient moins de {{ limit }} caractères ?',
+                        'min' => 2,
+                        'minMessage' => 'Le prénom contient moins de {{ limit }} caractères.',
                         'max' => 30,
-                        'maxMessage' => 'Le Prénom contient plus de {{ limit }} caractères ?'
+                        'maxMessage' => 'Le prénom contient plus de {{ limit }} caractères.',
                     ]),
                     new NotBlank([
-                        'message' => 'Veuillez renseigner un Prénom !'
+                        'message' => 'Veuillez renseigner un prénom.',
                     ]),
                     new Regex([
                         'pattern' => '/^[a-zA-ZÀ-ÿ\-\s]+$/u',
-                        'message' => 'Ce champs ne peut contenir que des lettres et tirets.'
+                        'message' => 'Ce champ ne peut contenir que des lettres, des espaces et des tirets.',
                     ]),
                 ],
                 'attr' => [
-                    'placeholder' => 'John'
+                    'placeholder' => 'John',
                 ],
-    
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom',
                 'constraints' => [
                     new Length([
-                        'min' => 2, 
-                        'minMessage' => 'Le Prénom contient moins de {{ limit }} caractères ?',
+                        'min' => 2,
+                        'minMessage' => 'Le nom contient moins de {{ limit }} caractères.',
                         'max' => 30,
-                        'maxMessage' => 'Le Prénom contient plus de {{ limit }} caractères ?'
+                        'maxMessage' => 'Le nom contient plus de {{ limit }} caractères.',
                     ]),
                     new NotBlank([
-                        'message' => 'Veuillez renseigner un Prénom !'
+                        'message' => 'Veuillez renseigner un nom.',
                     ]),
                     new Regex([
                         'pattern' => '/^[a-zA-ZÀ-ÿ\-\s]+$/u',
-                        'message' => 'Ce champs ne peut contenir que des lettres et tirets.'
+                        'message' => 'Ce champ ne peut contenir que des lettres, des espaces et des tirets.',
                     ]),
                 ],
                 'attr' => [
-                    'placeholder' => 'Doe'
+                    'placeholder' => 'Doe',
                 ],
             ])
-            ->add('role', ChoiceType::class, [
+            ->add('status', ChoiceType::class, [
                 'label' => 'Statut',
-                'choices'  => [
-                    'COMPTA' => "COMPTA",
-                    'COLLAB' => "COLLAB",
-                    'CLIENT' => "CLIENT",
+                'choices' => [
+                    'COMPTA' => 'COMPTA',
+                    'COLLAB' => 'COLLAB',
+                    'CLIENT' => 'CLIENT',
                 ],
                 'required' => false,
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer',
                 'attr' => [
-                    'class' => 'btn-alice-form'
-                ]
+                    'class' => 'btn-alice-form',
+                ],
             ]);
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/NewUserType.php
+++ src/Form/NewUserType.php
@@ -4,118 +4,113 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class NewUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email', EmailType::class, [
-            'label' => 'Email',
-            'attr' => [
-                'placeholder' => 'Entrez votre adresse email'
-            ],
-            'constraints' => [
-                new Length([
-                    'min' => 5, 
-                    'max' => 255,
-                    'minMessage' => 'Votre email contient moins de 5 caractères ?',
-                    'maxMessage' => 'Votre email est trop long !' 
-                ]),
-                new NotBlank([
-                    'message' => 'Veuillez renseigner votre adresse email.'
-                ]),
-                new Regex([
-                    'pattern' => '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/',
-                    'message' => 'L\'adresse email "{{ value }}" n\'est pas valide.',
-                ]),
-            ],
-            'invalid_message' => 'Veuillez saisir une adresse mail valide.'
-        ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse email',
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 5,
+                        'max' => 255,
+                        'minMessage' => 'Votre email contient moins de 5 caractères.',
+                        'maxMessage' => 'Votre email est trop long.',
+                    ]),
+                    new NotBlank([
+                        'message' => 'Veuillez renseigner votre adresse email.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/',
+                        'message' => 'L\'adresse email "{{ value }}" n\'est pas valide.',
+                    ]),
+                ],
+                'invalid_message' => 'Veuillez saisir une adresse mail valide.',
+            ])
             ->add('password', PasswordType::class, [
                 'label' => 'Mot de passe',
                 'attr' => [
-                    'placeholder' => 'Entrez un mot de passse provisoire'
+                    'placeholder' => 'Entrez un mot de passe provisoire',
                 ],
                 'constraints' => [
-                    // longueure min 8 max 20
                     new Length([
-                        'min' => 8, 
+                        'min' => 8,
                         'max' => 20,
-                        'minMessage' => 'Le mot depasse doit contenir au moins 8 caractères',
-                        'maxMessage' => 'Le mot depasse doit contenir moins de 20 caractères' 
+                        'minMessage' => 'Le mot de passe doit contenir au moins 8 caractères.',
+                        'maxMessage' => 'Le mot de passe doit contenir moins de 20 caractères.',
                     ]),
-                    // invalide si null
                     new NotBlank([
-                        'message' => 'Veuillez renseigner un mot de passe !'
+                        'message' => 'Veuillez renseigner un mot de passe.',
                     ]),
-                    // Oblige à entrer un MDP avec 8 à 20 char + 1 maj + 1 min + chiffre + caractere spé 
                     new Regex([
                         'pattern' => '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$/',
-                        'message' => 'Votre mot de passe doit contenir 1 majuscule, 1 minuscule, 1 caractère spécial, 1 chiffre et doit être composé de 8 à 20 caractères'
+                        'message' => 'Votre mot de passe doit contenir 1 majuscule, 1 minuscule, 1 caractère spécial, 1 chiffre et être composé de 8 à 20 caractères.',
                     ]),
                 ],
-                // TODO : revoir cette partie sur le MDP provisoire .............
             ])
             ->add('firstname', TextType::class, [
                 'label' => 'Prénom',
                 'constraints' => [
                     new Length([
-                        'min' => 2, 
-                        'minMessage' => 'Le Prénom contient moins de {{ limit }} caractères ?',
+                        'min' => 2,
+                        'minMessage' => 'Le prénom contient moins de {{ limit }} caractères.',
                         'max' => 30,
-                        'maxMessage' => 'Le Prénom contient plus de {{ limit }} caractères ?'
+                        'maxMessage' => 'Le prénom contient plus de {{ limit }} caractères.',
                     ]),
                     new NotBlank([
-                        'message' => 'Veuillez renseigner un Prénom !'
+                        'message' => 'Veuillez renseigner un prénom.',
                     ]),
                     new Regex([
                         'pattern' => '/^[a-zA-ZÀ-ÿ\-\s]+$/u',
-                        'message' => 'Ce champs ne peut contenir que des lettres et tirets.'
+                        'message' => 'Ce champ ne peut contenir que des lettres, des espaces et des tirets.',
                     ]),
                 ],
                 'attr' => [
-                    'placeholder' => 'John'
+                    'placeholder' => 'John',
                 ],
-    
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Nom',
                 'constraints' => [
                     new Length([
-                        'min' => 2, 
-                        'minMessage' => 'Le Prénom contient moins de {{ limit }} caractères ?',
+                        'min' => 2,
+                        'minMessage' => 'Le nom contient moins de {{ limit }} caractères.',
                         'max' => 30,
-                        'maxMessage' => 'Le Prénom contient plus de {{ limit }} caractères ?'
+                        'maxMessage' => 'Le nom contient plus de {{ limit }} caractères.',
                     ]),
                     new NotBlank([
-                        'message' => 'Veuillez renseigner un Prénom !'
+                        'message' => 'Veuillez renseigner un nom.',
                     ]),
                     new Regex([
                         'pattern' => '/^[a-zA-ZÀ-ÿ\-\s]+$/u',
-                        'message' => 'Ce champs ne peut contenir que des lettres et tirets.'
+                        'message' => 'Ce champ ne peut contenir que des lettres, des espaces et des tirets.',
                     ]),
                 ],
                 'attr' => [
-                    'placeholder' => 'Doe'
+                    'placeholder' => 'Doe',
                 ],
             ])
-            ->add('role', ChoiceType::class, [
+            ->add('status', ChoiceType::class, [
                 'label' => 'Statut',
-                'choices'  => [
-                    'COMPTA' => "COMPTA",
-                    'COLLAB' => "COLLAB",
-                    'CLIENT' => "CLIENT",
+                'choices' => [
+                    'COMPTA' => 'COMPTA',
+                    'COLLAB' => 'COLLAB',
+                    'CLIENT' => 'CLIENT',
                 ],
                 'required' => false,
             ])
@@ -125,9 +120,8 @@ class NewUserType extends AbstractType
                     'class' => 'btn-alice btn-alice-form',
                     'data-callback' => 'onSubmit',
                     'data-action' => 'submit',
-                ]
-            ])
-        ;
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
